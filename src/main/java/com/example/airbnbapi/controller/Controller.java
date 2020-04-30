c2b3214ba0f43abe953374f42fd3a7c87a4c3f19
JoshFen95 <jfennell@companieshouse.gov.uk>
2020-04-30 14:58:19 +0100

    add put methods, add repoistory factory, remove service factory

--- src/main/java/com/example/airbnbapi/controller/Controller.java
+++ src/main/java/com/example/airbnbapi/controller/Controller.java
@@ -2,15 +2,20 @@ package com.example.airbnbapi.controller;
 
 import com.example.airbnbapi.model.Media;
 import com.example.airbnbapi.model.MediaType;
-import com.example.airbnbapi.service.ServiceFactory;
+import com.example.airbnbapi.service.MediaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 import static org.springframework.http.ResponseEntity.status;
 
@@ -24,39 +29,60 @@ import java.util.Optional;
 public class Controller {
 
     @Autowired
-    private  ServiceFactory serviceFactory;
+    private MediaService mediaService;
 
 
-    // NEED TO ADD A PUT METHOD
+    // Add new item
+    @PostMapping(path = "/{type}/add")
+    public ResponseEntity<Media> addItem(@PathVariable("type") MediaType type, @RequestBody Media item) {
 
 
+        return status(HttpStatus.OK).body(mediaService.insertOrUpdateItem(type, item));
+    }
+
+    // Update exiting item via ID
+    @PutMapping(path = "/{type}/update/id/{id}")
+    public ResponseEntity updateItem(@PathVariable("type") MediaType type, @PathVariable("id") String id, @Valid @RequestBody Media itemToUpdate) {
+
+        Optional<? extends Media> searchedItem = mediaService.getItemById(type,id);
+
+        if (searchedItem.isPresent()) {
+
+            return status(HttpStatus.OK).body(searchedItem.get().getTitle() + " has been updated: " + mediaService.insertOrUpdateItem(type,itemToUpdate));
+        } else  {
+            return status(HttpStatus.NOT_FOUND).body("Item could not be found to update. New item added" + mediaService.insertOrUpdateItem(type,itemToUpdate));
+        }
+    }
+
+// Show all items of one type
     @GetMapping(path = "/{type}")
     public ResponseEntity getAll(@PathVariable("type") MediaType type) {
-        List<? extends Media> items = serviceFactory.getServiceByType(type).getItems();
+        List<? extends Media> items = mediaService.getItems(type);
 
         return status(HttpStatus.OK).body(items);
 
     }
 
+    // show an item via ID search
     @GetMapping(path = "/{type}/id/{id}")
     public ResponseEntity getById(@PathVariable("type") MediaType type, @PathVariable("id") String id) {
 
-        Optional<? extends Media> searchedItem = serviceFactory.getServiceByType(type).getItemById(id);
+        Optional<? extends Media> searchedItem = mediaService.getItemById(type,id);
 
         if (searchedItem.isPresent()) {
-            return status(HttpStatus.OK).body(serviceFactory.getServiceByType(type).getItemById(id));
+            return status(HttpStatus.OK).body(mediaService.getItemById(type, id));
         } else {
             return status(HttpStatus.NOT_FOUND).body("ID not found");
         }
     }
 
-
-    @DeleteMapping(path = "/delete/{type}/id/{id}")
+// Delete an item via ID search
+    @DeleteMapping(path = "/{type}/delete/id/{id}")
     public ResponseEntity deleteItemById(@PathVariable("type") MediaType type, @PathVariable("id") String id) {
 
 
-        if (serviceFactory.getServiceByType(type).getItemById(id).isPresent()) {
-            serviceFactory.getServiceByType(type).deleteById(id);
+        if (mediaService.getItemById(type, id).isPresent()) {
+            mediaService.deleteById(type, id);
             return status(HttpStatus.OK).body("id " + id + " has been deleted");
         } else {
             return status(HttpStatus.NOT_FOUND).body("id " + id + "  not found");
--- src/main/java/com/example/airbnbapi/model/Game.java
+++ src/main/java/com/example/airbnbapi/model/Game.java
@@ -1,5 +1,8 @@
 package com.example.airbnbapi.model;
 
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "games")
