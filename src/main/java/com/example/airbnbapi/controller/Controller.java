c367d7542dd0832c15168b19156e1d1d18fb6d24
JoshFen95 <jfennell@companieshouse.gov.uk>
2020-05-12 13:24:58 +0100

    remove test code

--- src/main/java/com/example/airbnbapi/controller/Controller.java
+++ src/main/java/com/example/airbnbapi/controller/Controller.java
@@ -41,15 +41,6 @@ public class Controller {
 
     private static final Logger logger = LoggerFactory.getLogger(Controller.class);
 
-    @GetMapping(path = "/log")
-    public String log() {
-        logger.trace("This is a TRACE level message");
-        logger.debug("This is a DEBUG level message");
-        logger.info("This is an INFO level message");
-        logger.warn("This is a WARN level message");
-        logger.error("This is an ERROR level message");
-        return "See the log for details";
-    }
 
     // Show all items of one type
     @GetMapping(path = "/log/{level}")
